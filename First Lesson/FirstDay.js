f0100b3b9d1b644f1e12398dea02017ba736eedf
DevArv <areventosrd@gmail.com>
2023-09-14 13:13:11 -0400

    First lesson completed.

--- First Lesson/FirstDay.js
+++ First Lesson/FirstDay.js
@@ -60,4 +60,78 @@ const login = {
 console.log("User: " + login.user + " password: " + login.password + " tocken: " + login.tocken);
 
 //Data types
-//Numbers //TODO
+//Numbers
+let employee = 23;
+let salary = 3650;
+let currency= 56.95;
+//Scientific notation
+let methers = 1e4; //> 10000
+let methersNegative = 1e-4; //> 0.0001
+
+//String
+const employeeRole = "Developer";
+const timeExperience = 1;
+
+console.log(`The role of this employee is ${employeeRole} and has ${timeExperience} year of experience`);
+
+let brand = "Iphone";
+
+console.log(brand.length); //> 6
+console.log(brand.toUpperCase()); //> IPHONE
+console.log(brand.toLowerCase()); //> iphone
+console.log(brand.substring(0,3)); //> iph
+
+//Objects
+const warrior = {
+    name: "Garen",
+    maxLevel: 18,
+    mana: false
+}
+
+console.log(warrior.name); //> Garen
+console.log(warrior.maxLevel); //> 18
+console.log(warrior.mana); //> false
+
+//Boolean
+const gravity = true;
+const planetEarth = false;
+
+//Type of data
+function leagueChampion(champion) {
+
+    return typeof champion;
+}
+
+console.log(leagueChampion("Shaco"), //> String
+            leagueChampion(18), //> Number
+            leagueChampion(false)); //> Boolean
+
+//Null            
+const namaConsume = null;
+
+console.log(namaConsume); //> Null
+
+//Undefined
+let newChampion;
+
+console.log(newChampion); //> undefined
+
+//Symbol
+let roleLeague = Symbol()
+
+const jungleChampion = {
+    name: "amumu",
+    mana: true,
+    [roleLeague]: "jungle",
+    maxLevel: 18
+}
+
+console.log(jungleChampion[roleLeague]); //> jungle
+
+//BigInt
+const distanceToDestination = 1696249n;
+
+console.log(distanceToDestination + 1n); //> 1696250n
+console.log(distanceToDestination - 1n); //> 1696248n
+console.log(distanceToDestination * 2n); //> 3392498n
+console.log(distanceToDestination / 2n); //> 849124n
