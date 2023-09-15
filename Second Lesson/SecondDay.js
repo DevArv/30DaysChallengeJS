9e5d0601a388a1c23c3e490404bff1103f15fced
DevArv <areventosrd@gmail.com>
2023-09-15 14:06:10 -0400

    Update: Operators

---
+++ Second Lesson/SecondDay.js
@@ -0,0 +1,23 @@
+//Arithmetic operators
+console.log(31 + 9); //> (Sum) 40
+console.log(31 - 9); //> (Difference) 22
+console.log(31 * 9); //> (Product) 279
+console.log(31 / 9); //> (Quotient) 3.9166666666666665
+console.log(31 % 9); //> (Remainder) 3
+
+//Assgination operators
+let x = 7;
+x += 3;
+console.log(x); //> 10
+
+x -= 3;
+console.log(x); //> 7
+
+x *= 3;
+console.log(x); //> 21
+
+x /= 3;
+console.log(x); //> 5.666666666666666
+
+x %= 3;
+console.log(x); //> 2
