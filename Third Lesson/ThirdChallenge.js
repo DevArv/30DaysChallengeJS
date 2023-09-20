476b7989266c3f474a50322f5e34c12bbde897da
DevArv <areventosrd@gmail.com>
2023-09-20 14:00:19 -0400

    Second challenge of third day in process.

--- Third Lesson/ThirdChallenge.js
+++ Third Lesson/ThirdChallenge.js
@@ -21,3 +21,85 @@
 
 // Input: 1984.25;
 // Output: false;
+
+function isLeapYear(year) {
+    if (year >= 0) {
+        if ((year % 4 === 0 && year % 100 !== 0) || (year % 100 === 0 && year % 400 === 0)){
+        return true;
+        } else {
+        return false;
+        }
+    } else {
+      return false;
+    }
+    
+  }
+  
+  console.log(isLeapYear(2000));
+  console.log(isLeapYear(-2024));
+  console.log(isLeapYear(1984.25));
+
+// En este desafío recibirás una serie de tipos de mascotas junto con su edad.
+
+// Dependiendo de estos 2 factores tendrás que construir la función llamada getPetExerciseInfo la cual retornará una cadena de texto 
+//con la información necesaria acerca de cuanto ejercicio necesita hacer cada tipo de mascota.
+
+// La función recibirá las siguientes mascotas:
+
+// perro
+// gato
+// ave
+// En caso de pasar una mascota la cual no sea de la lista deberá retornar "Tipo de mascota inválida"
+
+// Para cada tipo de mascota, la función retornará diferente información basada en la edad.
+
+// Perros
+// Si la edad es menor al año, deberá retornar "Los cachorros necesitan pequeñas y frecuentes sesiones de juego"
+// Si la edad es entre 1 y 7 años, deberá retornar "Los perros a esta edad necesitan caminatas diarias y sesiones de juego"
+// Si la edad es mayor a 7 años, deberá retornar "Los perros viejos necesitan caminatas más cortas"
+// gatos
+// Si la edad es menor al año, deberá retornar "Los gatitos necesitan frecuentes sesiones de juego"
+// Si la edad es entre 1 y 7 años, deberá retornar "Los gatos a esta edad necesitan jugar diariamente"
+// Si la edad es mayor a 7 años, deberá retornar "Los gatos viejos necesitan sesiones de juego más cortas"
+// aves
+// Si la edad es menor al año, deberá retornar "Las aves jóvenes necesitan mucho espacio para volar"
+// Si la edad es entre 1 y 7 años, deberá retornar "Las aves necesitan jugar diariamente y un lugar para volar"
+// Si la edad es mayor a 7 años, deberá retornar "Las aves mayores necesitan descansar más, pero siguen ocupando un lugar para volar"
+// Tendrás inputs y outputs como los siguientes 👇
+
+// Ejemplo 1:
+
+// Input: getPetExerciseInfo("perro", 3)
+// Output: "Los perros a esta edad necesitan caminatas diarias y sesiones de juego"
+
+// Ejemplo 2:
+
+// Input: getPetExerciseInfo("gato", 8)
+// Output: "Los gatos viejos necesitan sesiones de juego más cortas"
+
+// Ejemplo 3:
+
+// Input: getPetExerciseInfo("Mamut", 25)
+// Output: "Tipo de mascota invalida"
+
+function getPetExerciseInfo(type, age) {
+    switch (type) {
+        case "perro":
+            if (age <= 1) {
+                console.log("Los cachorros necesitan pequeñas y frecuentes sesiones de juego");
+            }else if (age <= 7) {
+                console.log("Los perros a esta edad necesitan caminatas diarias y sesiones de juego");
+            }else if (age > 7) {
+                console.log("Los perros viejos necesitan caminatas más cortas");
+            }else {
+                console.log("El perro no es un cachorro");
+            }
+            break;
+        default:
+            console.log("Tipo de mascota inválida");
+    }
+}
+
+getPetExerciseInfo("perro", 1);
+getPetExerciseInfo("perro", 5);
+getPetExerciseInfo("perro", 9);
