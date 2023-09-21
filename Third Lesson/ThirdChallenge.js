2e806904f64352d3185e49ab159885c54096448f
DevArv <areventosrd@gmail.com>
2023-09-21 13:46:56 -0400

    Third challenge, third day completed.

--- Third Lesson/ThirdChallenge.js
+++ Third Lesson/ThirdChallenge.js
@@ -120,3 +120,49 @@ console.log(getPetExerciseInfo("dog", 3));
 console.log(getPetExerciseInfo("cat", 8));
 console.log(getPetExerciseInfo("mamut", 25));
 console.log(getPetExerciseInfo("bird", 1));
+
+//En este desafío, debes dibujar un triángulo isósceles usando bucles.
+
+//Recibirás dos parámetros: size y character, que definen el tamaño y 
+//el carácter con el que se debe construir el triángulo, respectivamente. 
+//Además, el triángulo debe estar alineado a la derecha, 
+//lo que significa que la columna más derecha del triángulo debe estar en el borde derecho de la consola.
+
+//Recuerda que para hacer el salto de línea debes usar "\n", no olvides removerla de la última parte.
+
+//Tendrás inputs y outputs como los siguientes 👇
+
+//Ejemplo 1:
+
+//Input: printTriangle(5, "*")
+//Output:
+//     *
+//    **
+//   ***
+//  ****
+// *****
+
+// Ejemplo 2:
+
+// Input: printTriangle(6, "$")
+// Output:
+//      $
+//     $$
+//    $$$
+//   $$$$
+//  $$$$$
+// $$$$$$
+
+function printTriangle(size, character) {
+    const triangle = [];
+    for (let i = 1; i <= size; i++) {
+      let spaces = " ".repeat(size - i);
+      let characters = character.repeat(i);
+      let figure = `${spaces}${characters}`;
+      triangle.push(figure);
+    }
+    return triangle.join("\n");
+}
+
+console.log(printTriangle(5, "*"));
+console.log(printTriangle(6, "$"));
