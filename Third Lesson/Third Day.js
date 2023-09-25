d7cc457b134b34f78d0e2f2a55c3bcf51e94c20e
DevArv <areventosrd@gmail.com>
2023-09-25 13:56:34 -0400

    Update, pendiente ejemplos while.

--- Third Lesson/Third Day.js
+++ Third Lesson/Third Day.js
@@ -21,7 +21,7 @@ function needMana(champion) {
             return "This champion need mana to cast abilities.";
         case "Vladimir":
             return "This champion need blood to cast abilities.";
-            break;
+        break;
         default:
             return "This champion dont exist in the game.";
     }
@@ -29,3 +29,51 @@ function needMana(champion) {
 console.log(needMana("Garen"));
 console.log(needMana("Lux"));
 console.log(needMana("Vladimir"));
+
+//Example (2)
+function grabWeapon (character) {
+    switch (character) {
+        case "valeria":
+            return "Will has selected Valeria as a master sword.";
+        case "boomerang":
+            return "Will has selected a boomerang as a weapon, how boring you are dear player.";
+        break;
+        default:
+            return "You need a weapon if you want to go to the next room.";
+    }
+}
+
+console.log(grabWeapon("sword"));
+console.log(grabWeapon("boomerang"));
+console.log(grabWeapon());
+
+//Cicles
+for (i = 1; i <= 8 ; i++){
+    console.log(i);
+}
+//> 1, 2, 3, 4, 5.
+
+//For-in example
+const mainCharacter = {
+    name : "Will",
+    equipment : true,
+    inventory : true,
+    standardHealth : 3,
+}
+
+for (const prop in mainCharacter) {
+    console.log(mainCharacter[prop]);
+}
+
+//For-of example
+const charactersFirstAct = ["Will", "Valeria - normal", "Valeria - sword", "slimes"];
+
+for (const element of charactersFirstAct) {
+    console.log(element);
+}
+
+//While example
+let i = 2;
+while (i <= 20) {
+    
+}
