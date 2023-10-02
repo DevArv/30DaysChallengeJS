ff108a6cbc41e0f23349321c823d15a8d588bf1d
DevArv <areventosrd@gmail.com>
2023-10-02 13:22:54 -0400

    Update: new example of the challenge one 4th day

--- Fourth Lesson/FourthDay.js
+++ Fourth Lesson/FourthDay.js
@@ -19,7 +19,7 @@ const localPrice = price.reduce(function(tax, price) {
 }, 0);
 console.log(localPrice); //> 5.9
 
-//Additional example of the first challenge in the fourth day.
+//Example 1 of the first challenge in the fourth day.
 
 const championLeague = [
     {name: "Shaco", Damage: [240, 309]},
@@ -54,3 +54,39 @@ function findMaxDamage(championLeague) {
 
 const maxDamageChampionName = findMaxDamage(championLeague);
 console.log("The champion with the highest damage is:", maxDamageChampionName);
+
+//Example 2 of the first challenge in the fourth day.
+
+const stepsWeek = [
+    {name: "Alex", steps: [1800, 2000, 3190]},
+    {name: "Maria", steps: [1200, 4600, 1800]},
+    {name: "Jose", steps: [900, 3200, 1200]},
+];
+
+function findMaxSteps(stepsWeek) {
+    let maxStepsUser = {
+        name: [],
+        maxSteps: 0,
+    }
+
+    for (let i = 0; i < stepsWeek.length; i++) {
+        const user = stepsWeek[i];
+
+        const totalSteps = user.steps.reduce(
+            (total, current) => total + current, 0);
+
+    if (totalSteps === maxStepsUser.maxSteps) {
+        maxStepsUser.name.push(user.name);
+    }
+
+    if (totalSteps > maxStepsUser.maxSteps) {
+        maxStepsUser.maxSteps = totalSteps;
+        maxStepsUser.name = [];
+        maxStepsUser.name.push(user.name);
+    }
+        return maxStepsUser.name;
+    }
+}
+
+const maxStepsUserName = findMaxSteps(stepsWeek);
+console.log("The user with the highest steps is:", maxStepsUserName);
