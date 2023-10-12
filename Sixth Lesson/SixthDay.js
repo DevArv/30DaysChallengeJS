585620297a4da61989e17f576951817ae45d53a2
DevArv <areventosrd@gmail.com>
2023-10-12 14:26:30 -0400

    Update: Sixth day completed.

---
+++ Sixth Lesson/SixthDay.js
@@ -0,0 +1,27 @@
+//Reassignment
+
+let attributeType = 'fire';
+attributeType = 'water';
+
+console.log(attributeType);
+
+//Cannot change a declaration when it has been declarated as a constant
+// const maxLevel = 18;
+// maxLevel = 19;
+
+
+//Redeclaration
+// let maxHealth = 100;
+// let maxHealth = 200;
+
+var matchType = 'Ranked';
+var matchType = 'Aram';   //> This could work
+
+// 'use strict demo'
+
+// 'use strict';
+
+// let playerName = 'GGA Riku';
+// let playerName = 'Kakaroto';
+
+// console.log(playerName);
