c4337a10fb56f496b84ac126ef936da4ec536279
DevArv <areventosrd@gmail.com>
2023-11-09 13:42:35 -0400

    Update: Higher order functions examples

--- Eighth Lesson/EighthDay.js
+++ Eighth Lesson/EighthDay.js
@@ -29,3 +29,21 @@ console.log(double(10));
 const triple = multiplyBy(3);
 console.log(triple(5));
 console.log(triple(10));
+
+//Higher order functions
+//Example 1
+const actualLevelTeam = [8, 12, 7, 10, 14];
+const expectedLevelTeam = actualLevelTeam.map(function(level){
+    return level * 2;
+});
+
+console.log(expectedLevelTeam);
+console.log(actualLevelTeam); //Array is not modified
+
+//Example 2
+const championLevel = [8, 12, 7, 10, 14];
+const halfLevel = championLevel.filter(function(experience){
+    return experience % 2 === 0;
+});
+
+console.log(halfLevel); //> [8, 12, 10, 14]
