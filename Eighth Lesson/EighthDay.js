aa778e6410bae35f835798fc57f7d7e9a2fc228e
DevArv <areventosrd@gmail.com>
2023-11-15 13:18:00 -0400

    Update: Repaso ultimo challenge, dia 8

--- Eighth Lesson/EighthDay.js
+++ Eighth Lesson/EighthDay.js
@@ -49,3 +49,36 @@ const halfLevel = championLevel.filter(function(experience){
 console.log(halfLevel); //> [8, 12, 10, 14]
 
 //Challenge (2) example.
+// Definition of the mainMap function that takes an array and a function as arguments
+function mainMap(array, func) {
+    // Create a new empty array to store the results
+    var mainArray = [];
+
+    // Iterate through each element of the input array
+    for (var i = 0; i < array.length; i++) {
+        // Apply the provided function to each element and add the result to the new array
+        mainArray.push(func(array[i]));
+    }
+
+    // Return the new array with the results
+    return mainArray;
+}
+
+// Define an array of objects representing players with properties 'name' and 'point'.
+const playerArray = [
+    {name: "Alex", point: 90},
+    {name: "Maria", point: 93},
+    {name: "Pedro", point: 87}
+];
+
+// Use the mainMap function to obtain a new array of player names.
+const playerNames = mainMap(playerArray, (players) => players.name);
+
+// Use the mainMap function to obtain a new array with the players points.
+const playerPoints = mainMap(playerArray, (players) => players.point);
+
+// Print the new array with the players names
+console.log(playerNames); //> ["Alex", "Maria", "Pedro"]
+
+// Print the new array with the players points
+console.log(playerPoints); //> [90, 93, 87]
