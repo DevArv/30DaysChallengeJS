e3505c4af601e8c11eb9463c9e9eecbe339993fd
DevArv <areventosrd@gmail.com>
2023-12-06 13:58:44 -0400

    Update: Challenge 2 day 11

--- Eleventh Lesson/EleventhChallenge.js
+++ Eleventh Lesson/EleventhChallenge.js
@@ -86,3 +86,69 @@ sendEmail("Hi, good luck with this challenge", "You can do it!")
     console.error(error.message);
 })
 
+// En este desafío tienes un código base el cual se aplica una mala práctica de programación 
+//que conocemos como el callback hell y tu reto es evitarlo usando promesas.
+
+// Nota: No uses async/await, ya que este reto se trata de resolverlo mediante promesas con la función then.
+
+// Para solucionarlo vas a encontrar una función llamada runCode que no recibe parámetros de entrada 
+//que tiene el código base que tienes que refactorizar, además el archivo tasks.js con las funciones 
+//que tienes que ejecutar.
+
+// Dentro del cuerpo de la función runCode debes escribir tu solución y además pasar y 
+//deberías pasar las funciones del archivo tasks.js a promesas.
+
+// Ejemplo:
+
+// Input:
+// runCode()
+// .then(response => console.log(response));
+
+// Output:
+// ["Task 1", "Task 2", "Task 3"]
+
+//import { doTask1, doTask2, doTask3 } from './tasks';
+
+// export function runCode() {
+//   const strings = [];
+//   return new Promise((resolve) => {
+//     doTask1((rta1) => {
+//       strings.push(rta1);
+//       doTask2((rta2) => {
+//         strings.push(rta2);
+//         doTask3((rta3) => {
+//           strings.push(rta3);
+//           resolve(strings);
+//         })
+//       })
+//     })
+//   })
+// }
+
+
+import { doTask1, doTask2, doTask3 } from './TasksChallenge.js';
+function runCode() {
+    const strings = [];
+  
+    return doTask1()
+        .then(resultTask1 => {
+        strings.push(resultTask1);
+        return doTask2();
+    })
+    .then(resultTask2 => {
+        strings.push(resultTask2);
+        return doTask3();
+    })
+    .then(resultTask3 => {
+        strings.push(resultTask3);
+        return strings;
+    })
+}
+
+runCode()
+    .then(result => {
+        console.log(result);
+    })
+    .catch(error => {
+        console.error(error);
+    });
