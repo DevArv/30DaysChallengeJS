b6ec47e053c0df88951e07152f2e07f04ea20cce
DevArv <areventosrd@gmail.com>
2023-12-14 13:41:31 -0400

    Update: Still imcomplete day 12.

--- Twelfth Lesson/TwelfthDay.js
+++ Twelfth Lesson/TwelfthDay.js
@@ -33,4 +33,30 @@ console.log(copiedDaysLeft);
 console.log(copiedDaysLeft === daysLeft);
 //output: false
 
-//TODO: Finish Arrays Lesson
+//Method concat
+let tenPlusNumber = [10, 20, 30];
+let secondTenPlusNumber = [40, 50, 60];
+let allPlusNumber = tenPlusNumber.concat(...secondTenPlusNumber);
+console.log(allPlusNumber);
+//output: [ 10, 20, 30, 40, 50, 60 ]
+
+//Spread Operator
+let spreadOperatorArrays = [...developers, ...daysLeft];
+console.log(spreadOperatorArrays);
+//output: [ 'Nikolay', 'Alex', 'Sergey', 'Dmitry', 15, 30, 45 ]
+
+//Bidimensional Arrays
+let matrix = [
+    [2, 4, 6],
+    [8, 10, 12],
+    [14, 16, 18]
+];
+
+console.log(matrix[2][2]);
+console.log(matrix[0][1]);
+console.log(matrix[1][0]);
+//output: 18
+//output: 4
+//output: 8
+
+//Todo: finish this Lesson
