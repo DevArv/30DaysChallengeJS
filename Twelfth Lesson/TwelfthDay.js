5537a4f75c67fbf30e1eb255fb43887cfa94b5d7
DevArv <areventosrd@gmail.com>
2023-12-14 23:31:06 -0400

    Update: Arrays completed.

--- Twelfth Lesson/TwelfthDay.js
+++ Twelfth Lesson/TwelfthDay.js
@@ -59,4 +59,42 @@ console.log(matrix[1][0]);
 //output: 4
 //output: 8
 
-//Todo: finish this Lesson
+//Bidimensional Arrays using constructor class
+for (let i = 0; i < matrix.length; i++) {
+    matrix[i] = new Array(3);
+}
+
+matrix[0][0] = 1;
+matrix[0][1] = 2;
+matrix[0][2] = 3;
+matrix[1][0] = 4;
+matrix[1][1] = 5;
+matrix[1][2] = 6;
+matrix[2][0] = 7;
+matrix[2][1] = 8;
+matrix[2][2] = 9;
+
+console.log(matrix);
+//output: [ [ 1, 2, 3 ], [ 4, 5, 6 ], [ 7, 8, 9 ] ]
+
+//Bidimensional Arrays using spread operator
+ const matrix2 = [[1, 2, 3], [4, 5, 6]];
+ const copiedMatrix2 = [...matrix2];
+
+ console.log(copiedMatrix2);
+ //output: [ [ 1, 2, 3 ], [ 4, 5, 6 ] ]
+
+ //Tridimensional Arrays
+ const triangle = [];
+ for (let i = 0; i < 3; i++) {
+    triangle[i] = [];
+    for (let j = 0; j < 3; j++) {
+        triangle[i][j] = [];
+        for (let k = 0; k < 3; k++) {
+            triangle[i][j][k] = i + j + k;
+        }
+    }
+ }
+
+ console.log(triangle);
+ //output: [ [ [ 0, 1, 2 ], [ 1, 2, 3 ], [ 2, 3, 4 ] ], [ [ 1, 2, 3 ], [ 2, 3, 4 ], [ 3, 4, 5 ] ], [ [ 2, 3, 4 ], [ 3, 4, 5 ], [ 4, 5, 6 ] ] ]
