2ea851bc2442f5529d9f1f158c54e0b796668690
DevArv <areventosrd@gmail.com>
2024-01-10 13:34:06 -0400

    feat: :sparkles: Challenge: function searchValue(array, value) {}

--- Thirteenth Lesson/ThirteenthChallenge.js
+++ Thirteenth Lesson/ThirteenthChallenge.js
@@ -73,3 +73,72 @@ const products = [
 ];
 
 console.log(groupProducts(products, "Electronics"));
+
+/* En este desafío, tu objetivo es encontrar un valor específico en un array de dos dimensiones.
+
+La función searchValue recibirá dos parámetros: un array bidimensional y un valor a buscar. 
+Tu tarea será implementar la lógica necesaria para encontrar el valor y retornar un objeto con las propiedades 
+row y column que indicarán la posición del valor dentro del array bidimensional.
+
+Si el valor no se encuentra en la matriz, la función deberá lanzar un error con el mensaje "Valor no encontrado". 
+Recuerda que la sintaxis para lanzar errores es la siguiente
+
+throw new Error("Valor no encontrado");
+
+Ejemplo 1:
+
+
+Input:
+
+const array = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+]
+
+const value = 5
+
+searchValue(array, value)
+
+Output:
+
+{
+  row: 1,
+  column: 1,
+}
+
+Ejemplo 2:
+
+
+Input:
+
+const array = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+
+const value = 45;
+
+Output: "Valor no encontrado" */
+
+function searchValue(array, value) {
+    for (let row = 0; row < array.length; row++) {
+        for (let column = 0; column < array[row].length; column++) {
+            if (array[row][column] === value) {
+                return {
+                    row: row,
+                    column: column,
+                }
+            }
+        }
+    }
+    throw new Error("Valor no encontrado");
+}
+
+try {
+    const result = searchValue(array, value);
+    console.log(result);
+} catch (error) {
+    console.error(error.message);
+}
