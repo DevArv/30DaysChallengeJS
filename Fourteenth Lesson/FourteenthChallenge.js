ea5be0229193d799cb76aa5450c948bfe9e87d69
DevArv <areventosrd@gmail.com>
2024-01-17 13:36:22 -0400

    docs: Doc: Sort challenge - pending

--- Fourteenth Lesson/FourteenthChallenge.js
+++ Fourteenth Lesson/FourteenthChallenge.js
@@ -1,3 +1,4 @@
+// Challenge #1
 /* En este desafío tendrás que procesar una lista de compras.
 
 Deberás implementar la lógica de la función processShoppingList de tal manera que esta módifique el array original de la siguiente manera
@@ -83,3 +84,41 @@ Output:
     { name: 'leche', price: 25 },
     { name: 'oferta manzanas', price: 24 }
 ] */
+
+// Challenge #2
+/*En este desafío, tendrás que ordenar una lista de productos.
+
+Tu tarea es implementar la lógica de la función sortByAvailabilityAndPrice. 
+Esta función recibirá un array de objetos que representan productos, 
+y devolverá una copia ordenada de dicho array.
+
+El ordenamiento se realizará siguiendo dos criterios:
+
+Primero, los productos disponibles en inventario serán colocados al principio de la lista.
+Luego, los productos serán ordenados por su precio, de manera ascendente.
+Es importante destacar que la lista original no sufrirá ninguna modificación 
+y que la función devolverá una nueva lista con los cambios mencionados.
+
+Ejemplo
+
+Input:
+
+const products = [
+  { name: "product1", price: 10, inStock: true },
+  { name: "product2", price: 20, inStock: false },
+  { name: "product3", price: 15, inStock: true },
+  { name: "product4", price: 5, inStock: false },
+]
+
+sortByAvailabilityAndPrice(products)
+
+Output:
+[
+  { name: "product1", price: 10, inStock: true },
+  { name: "product3", price: 15, inStock: true },
+  { name: "product4", price: 5, inStock: false },
+  { name: "product2", price: 20, inStock: false },
+] */
+
+function sortByAvailabilityAndPrice(products) {
+}
