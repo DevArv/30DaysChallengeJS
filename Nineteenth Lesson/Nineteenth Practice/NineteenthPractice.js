0aa2dbb68b2e98ca4141e921932465637af627be
DevArv <areventosrd@gmail.com>
2024-03-19 22:34:27 -0400

    ✨ feat: Inheritance
    Pratice

---
+++ Nineteenth Lesson/Nineteenth Practice/NineteenthPractice.js
@@ -0,0 +1,26 @@
+// Inheritance practice
+class Game {
+    constructor(platform) {
+        this.platform = platform;
+    }
+
+    isAvailable() {
+        console.log(`This game is available on ${this.platform}`);
+    }
+}
+
+class Riot extends Game {
+    constructor(platform, Game) {
+        super(platform);
+        this.Game = Game;
+
+        console.log(`The game ${this.Game} is available on ${this.platform}`);
+    }
+
+    needTools() {
+        console.log(`The game ${this.Game} required a keyboard and a mouse to play on ${this.platform}`);
+    }
+}
+
+const RIOT = new Riot("PC", "League of Legends");
+RIOT.needTools();
