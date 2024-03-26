3a7a4249b39f10a9cfdf80ce4c7f8890401e2840
DevArv <areventosrd@gmail.com>
2024-03-25 22:55:33 -0400

    ✨ feat(Implementacion de primeras clases, necesito repaso completo): En proceso

--- Nineteenth Lesson/NineteenthDay.js
+++ Nineteenth Lesson/NineteenthDay.js
@@ -25,3 +25,30 @@ class Cat extends Pet {
 const cat = new Cat("black", 2);
 cat.isWalking();
 cat.isEating();
+
+//Polimorphism
+class Movement {
+    move() {
+        console.log("Start moving");
+    }
+}
+
+class Walk extends Movement {
+    move() {
+        console.log("Now it's walking");
+    }
+}
+
+class Run extends Movement {
+    move() {
+        console.log("Now it's running");
+    }
+}
+
+const MOVEMENT = new Movement();
+const WALK = new Walk();
+const RUN = new Run();
+
+MOVEMENT.move();
+WALK.move();
+RUN.move();
